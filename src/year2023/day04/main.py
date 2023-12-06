d1c84eee4a9dd30f66e8ae6214e0c9d6592f76a3
Emanuel Ferm <emanuel@eferm.com>
2023-12-06 18:54:55 +0000

    add day 4 part 2

--- src/year2023/day04/main.py
+++ src/year2023/day04/main.py
@@ -10,8 +10,25 @@ def parse_sequence(string):
 
 def parse_card(line):
     wins, your = map(parse_sequence, line.split(":")[1].split("|"))
-    winning = wins & your
-    return 2 ** (len(winning) - 1) if len(winning) else 0
+    return wins & your
 
 
-print("Part 1:", sum(map(parse_card, lines)))
+def score1(numbers):
+    return 2 ** (len(numbers) - 1) if numbers else 0
+
+
+card_winners = list(map(parse_card, lines))
+print("Part 1:", sum(map(score1, card_winners)))
+
+
+def playthrough2(winners):
+    origs = dict(enumerate(map(len, winners), start=1))  # {1: 4, 2: 2, ...}
+    q = list(origs.keys())
+    while q:
+        card = q.pop()
+        q.extend(range(card + 1, card + origs[card] + 1))
+        yield card
+
+
+cards_played = list(playthrough2(card_winners))
+print("Part 2:", len(cards_played))
