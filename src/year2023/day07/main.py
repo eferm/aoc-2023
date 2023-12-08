5d870189ab5ef38b025a157cf3c7ee011dbc8fea
Emanuel Ferm <emanuel@eferm.com>
2023-12-08 01:15:38 +0000

    add day 7 part 2

--- src/year2023/day07/main.py
+++ src/year2023/day07/main.py
@@ -9,11 +9,18 @@ def parse_line(string):
     return {"cards": t[0], "bid": int(t[1])}
 
 
-def card_strength(card):
-    return "AKQJT98765432".index(card)
+alpha = "abcdefghijklmnopqrstuvwxyz"
 
 
-def type_strength(hand):
+def card_strength1(card):
+    return alpha["AKQJT98765432".index(card)]
+
+
+def card_strength2(card):
+    return alpha["AKQT98765432J".index(card)]
+
+
+def type_strength1(hand):
     counts = Counter(hand["cards"])
     if max(counts.values()) == 5:
         return 0
@@ -30,18 +37,58 @@ def type_strength(hand):
     return 6
 
 
-def strength(hand):
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    card_strengths = map(card_strength, hand["cards"])
-    card_strengths_str = map(lambda i: alpha[i], card_strengths)  # make 1 char
-    return str(type_strength(hand)) + "".join(card_strengths_str)
+def type_strength2(hand):
+    hand = hand["cards"]
+    print(hand, end=" -> ")
+
+    if hand == "JJJJJ":
+        hand = "AAAAA"
+    elif "J" in hand:
+        sansj = hand.replace("J", "")
+
+        def sortkey(c):
+            return str(4 - Counter(sansj).get(c)) + card_strength2(c)
+
+        replacement = sorted(sansj, key=sortkey)[0]
+        # print(candidates, end=" ")
+        hand = hand.replace("J", replacement)
+    print(hand)
+
+    counts = Counter(hand)
+    if max(counts.values()) == 5:
+        return 0
+    if max(counts.values()) == 4:
+        return 1
+    if counts.most_common()[0][1] == 3 and counts.most_common()[1][1] == 2:
+        return 2
+    if max(counts.values()) == 3:
+        return 3
+    if counts.most_common()[0][1] == 2 and counts.most_common()[1][1] == 2:
+        return 4
+    if max(counts.values()) == 2:
+        return 5
+    return 6
 
 
-def score(hands):
-    ranked = enumerate(sorted(hands, key=strength, reverse=True), start=1)
+def strength1(hand):
+    card_strengths = map(card_strength1, hand["cards"])
+    return str(type_strength1(hand)) + "".join(card_strengths)
+
+
+def strength2(hand):
+    card_strengths = map(card_strength2, hand["cards"])
+    return str(type_strength2(hand)) + "".join(card_strengths)
+
+
+def score(ranked):
     for rank, hand in ranked:
         yield rank * hand["bid"]
 
 
-hands = map(parse_line, lines)
-print("Part 1:", sum(score(hands)))
+hands = list(map(parse_line, lines))
+
+ranked = enumerate(sorted(hands, key=strength1, reverse=True), start=1)
+print("Part 1:", sum(score(ranked)))
+
+ranked = enumerate(sorted(hands, key=strength2, reverse=True), start=1)
+print("Part 2:", sum(score(ranked)))
