c7769aa3451e160c2ecaf23e346d511b2aa85195
Emanuel Ferm <emanuel@eferm.com>
2023-12-10 13:54:23 +0000

    add day 8 part 2

--- src/year2023/day08/main.py
+++ src/year2023/day08/main.py
@@ -1,7 +1,7 @@
+import functools
 import itertools
+import math
 import re
-
-from utils import *
 
 with open("src/year2023/day08/input.txt") as f:
     chunks = f.read().split("\n\n")
@@ -20,16 +20,23 @@ def parse_nodes(string):
     }
 
 
-def navigate(instructions, nodes):
-    curr = "AAA"
-    for instr in itertools.cycle(instructions):
-        if curr == "ZZZ":
-            return
-        yield curr
-        curr = nodes[curr][instr]
+def navigate(instr, nodes, ends, start):
+    curr = start
+    for i in itertools.cycle(instr):
+        if curr in ends:
+            return curr
+        yield 1
+        curr = nodes[curr][i]
 
 
-instructions = chunks[0]
+instr = chunks[0]
 nodes = parse_nodes(chunks[1])
-# print(nodes)
-print(len(list(navigate(instructions, nodes))))
+
+navigate1 = functools.partial(navigate, instr, nodes, ["ZZZ"])
+print("Part 1:", sum(navigate1("AAA")))
+
+
+starting = [k for k in nodes if k.endswith("A")]
+ending = [k for k in nodes if k.endswith("Z")]
+navigate2 = functools.partial(navigate, instr, nodes, ending)
+print("Part 2:", math.lcm(*map(sum, map(navigate2, starting))))
